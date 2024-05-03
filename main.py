1efc71a2def44efa060047733f3092c6066a7079
CharlesEOhagin <charles.ohagin@gmail.com>
2024-05-03 12:25:56 -0700

    added problem class and started on user inputs

---
+++ main.py
@@ -0,0 +1,6 @@
+from state import State
+from problem import Problem
+
+print("Welcome to XXX (change this to your student ID) 8 puzzle solver.")
+option  = input( "Type “1” to use a default puzzle, or “2” to enter your own puzzle.")
+print(option)
---
+++ problem.py
@@ -0,0 +1,6 @@
+from state import State
+
+def Problem():
+    def __init__(self,start_state,goal_state):
+        self.start_state = start_state
+        self.goal_state = goal_state
