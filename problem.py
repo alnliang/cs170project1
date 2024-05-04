4d21ad95faa0da8118dd40ce656855451b885d94
CharlesEOhagin <charles.ohagin@gmail.com>
2024-05-03 18:17:10 -0700

    made problem class

--- problem.py
+++ problem.py
@@ -1,6 +1,6 @@
 from state import State
 
-def Problem():
+class Problem:
     def __init__(self,start_state):
         self.start_state = start_state
         self.goal_state =[[1,2,3],[4,5,6],[7,8,0]]
